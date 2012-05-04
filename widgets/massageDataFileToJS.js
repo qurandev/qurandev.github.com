43459a07045ce079feb40bdd739b313e034380b0
Q D <qurandev@gmail.com>
2012-05-04 08:14:31 +0530

    misc stuff

---
+++ widgets/massageDataFileToJS.js
@@ -0,0 +1,39 @@
+﻿var fs = require('fs');
+//var request = require('request');
+var cheerio = require('cheerio');
+
+var body = '';
+$ = cheerio.load(body);
+
+fs.readdir( "./data/", function( err, files) {
+    if ( err ) {
+        console.log("Error reading files: ", err);
+    } else { //console.log( files );
+        $.each( files, function(fileno, file){ 
+            if(!/\.txt$/.test( file ) ) return; //only process .txt files!
+            var type = file.split('.txt')[0].toLowerCase().replace(/^q/, '');
+            console.log('Processing TYPE: \t' + type +'\t\tfrom File: ' + file);
+            file = "data/" + file;
+            fs.readFile(file, "utf-8", function(err, data){
+                if(err){
+                    console.log(err);
+                } else{
+                    var result = "", map = {};
+                    console.log(file + " file was read");
+
+                    $.each( data.split('\n'), function(lineno, line){
+                        result += line.replace(/\"/g, "\\\"") + "\\n\\\n";
+                    });
+                    result = "var type='" + type + "', _DATA = \"" + result + "\"; gq.cookdata(type, _DATA);";
+                    fs.writeFile(file + "_script.js", result, function(err) {
+                        if(err) {
+                            console.log(err);
+                        } else {
+                            console.log(file + " info was saved!");
+                        }
+                    });
+                }
+            });
+        });
+    }
+});
