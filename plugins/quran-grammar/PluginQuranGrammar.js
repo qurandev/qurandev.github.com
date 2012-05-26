5e2cfcbe5ad57b0998e807932a61f1776cc43b74
Q D <qurandev@gmail.com>
2012-05-26 13:08:39 +0530

    cosmetic + book embed using pdf parts

--- plugins/quran-grammar/PluginQuranGrammar.js
+++ plugins/quran-grammar/PluginQuranGrammar.js
@@ -103,48 +103,37 @@ var CORPUS = {
 	
 	UIgetNearSynonymsPageLink: function(lemma, pageno){
 		var TEMPLATE = '<A HREF="javascript:CORPUS.UIsynonymClicked(\'$LEM\', $PAGENO, $1);" TARGET=_>$2</A>';
-		//'<A HREF="javascript:CORPUS.UIsynonymClicked(\'$LEM\', $2, \'http://www.scribd.com/embeds/82681420/content?access_key=key-6w25dij9keuw0vv8keu&amp;start_page=$1\')" TARGET=_ >$2</A>';
 		var mapPageno = pageno ? ( 18 + parseInt(pageno) ) : 1;
 		var linktext = pageno ? 'Page# ' + parseInt(pageno) : 'Book';
 		return TEMPLATE.replace(/\$1/g, mapPageno ).replace(/\$2/g, linktext ).replace(/\$LEM/g, lemma).replace(/\$PAGENO/g, pageno);
 	},
-	
-	_scribd_doc: null,
-	
-	UIsynonymClicked: function(lemma, pageno, mappageno){ //var iframeURL = 'http://www.scribd.com/embeds/82681420/content?access_key=key-6w25dij9keuw0vv8keu&amp;start_page=$1'; iframeURL = iframeURL.replace(/\$1/g, mappageno);
+
+	UIsynonymClicked: function(lemma, pageno, mappageno){
+		var mappedpageno, offset, temp, width, height, BOOKLINK = "<A HREF='$URL' TARGET=_>Link to PDF of this part</A><BR><BR><A HREF='http://archive.org/details/MutaraadifaatUlQuran-UrduToEnglishProject-10PageSplitsOfficial' TARGET=_>Link to all PDF parts.</A>", URL = "http://ia600405.us.archive.org/16/items/MutaraadifaatUlQuran-UrduToEnglishProject-10PageSplitsOfficial/Mutaradifaat-ul-Quran-COMPLETE.$PAGE.pdf#page=$OFFSET";
+		mappedpageno = 1 + parseInt( (pageno + 18) / 10 );
+		offset = (pageno + 18) - 10*(mappedpageno-1);
+		temp = 100000 + mappedpageno;
+		temp = (temp + '').substring(1);
+		URL = URL.replace(/\$PAGE/g, temp).replace(/\$OFFSET/g, offset); console.log(pageno +' '+ mappedpageno +' ' + offset + ' ' + URL);
+		var embedPDF = function(){
+			var myPDF = new PDFObject({ 
+			  url: URL
+			}).embed('embedded_doc');
+			$('#book').hide();
+			$.blockUI({ message: $('#book') }); 
+			$('.blockOverlay').attr('title','Click to unblock').click( function(){ $.unblockUI(); $('#book').hide(); } );
+			$("#bookclose, #bookclose2").click(function() { $.unblockUI(); $('#book').hide(); });
+		}
 		if( !$('#book') || $('#book').length <= 0 ){
-			loadExtraFiles('http://www.scribd.com/javascripts/scribd_api.js');
-			$('body').append('<div id=book style="position:fixed; bottom:0; left:0;"><div style="background-color: #32BD2F; height: 12px;"><div style="float: right; width: 42px; height: 12px; padding: 2px;"><img id="bookclose" alt="Close" src="images/close.gif" /></div></div><div id="embedded_doc" class=leftcolumn><a href="http://www.scribd.com" target=_>Scribd</a></div></div></div>');
-			CORPUS.UILoadPDF(mappageno);
+			$('body').append('<div id=book style="display: none;border: 4px solid #69F;height: 85%; width: 40%; position:fixed; bottom:0; left:0;"><div style="background-color: #32BD2F; height: 12px;"><div style="float: right; width: 42px; height: 12px; padding: 2px;"><img id="bookclose" alt="Close" src="images/close.gif" /></div></div><div id="embedded_doc" class=leftcolumn><img src="images/loading.gif" /></div></div></div>');
+			$.getScript( 'javascript/pdfobject.js', function(){
+				embedPDF();
+			});
 		}
-		else{ //navigate to the specified page.. mappageno
-			$('#book').show();
-			if(CORPUS._scribd_doc){
-				CORPUS._scribd_doc.api.setPage( mappageno );
-			}
+		else{ $('#book').show(); embedPDF(); //if page# has changed???
 		}
-		$("#bookclose, #bookclose2").click(function() { $('#book').hide(); /*$.unblockUI();*/ });
-		/*$.blockUI({ message: $("#book"), css: {
-			 Xwidth: '425px',
-			 Xheight: '225px',
-			 Xleft: ($(window).width() - 425) /2 + 'px',
-			 Xtop: '10%'
-		}});*/
+		//$('#results').html( BOOKLINK.replace(/\$URL/, URL ) );
 		return false;
-	},
-	
-	UILoadPDF: function(mappageno){
-		if(typeof(scribd) == 'undefined' || !scribd || !scribd.Document){
-			setTimeout('CORPUS.UILoadPDF('+ mappageno + ')', 100); //TODO: there must be max number of attempts!!!
-			return;
-		}
-		var scribd_doc  = scribd.Document.getDoc(82681420, 'key-6w25dij9keuw0vv8keu');
-		var onDocReady = function(e){ /*alert('document pdf ready');*/ $('#book').show(); if(scribd_doc) scribd_doc.api.setPage(mappageno); CORPUS._scribd_doc = scribd_doc; }
-		if(scribd_doc){ scribd_doc.addParam('jsapi_version', 1); scribd_doc.addEventListener('docReady', onDocReady); 
-			scribd_doc.addParam('height', 550);  scribd_doc.addParam('width', 425); scribd_doc.addParam('auto_size', true);
-			scribd_doc.addParam('mode', 'list'); scribd_doc.addParam('jsapi_version', 2);
-			scribd_doc.write('embedded_doc'); $('#book').show(); // Write the instance
-		}
 	},
 	
 	UIgetSarfSagheer: function(root, form){
